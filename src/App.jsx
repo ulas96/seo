a0bb23daab918ca27e3a8478c78319ad4b427b32
ulas96 <y.ulas.yildiz@gmail.com>
2024-05-06 03:40:27 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import axios from "axios";
 
@@ -7,30 +7,57 @@ import axios from "axios";
 
 
 function App() {
-  const [response, setResponse] = useState({});
-  const [id,setId] = useState("");
+  const [response, setResponse] = useState();
+  const [url,setUrl] = useState("");
+  const [id,setId] = useState("")
 
   const handleChange = (e) => {
-    setId(getYoutubeVideoId(e.target.value));
+    setUrl(e.target.value);
+    setId(getYoutubeVideoId(url))
   }
 
-  function getYoutubeVideoId(url) {
+  const getYoutubeVideoId = (url) => {
     const pattern = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const match = url.match(pattern);
     return match ? match[1] : null;
 }
 
+  const sendId = (videoId) => {
+    try {
+      const confirmation = axios.post(`https://sdgvr2g6eh.execute-api.eu-north-1.amazonaws.com/p/bin?videoid=${videoId}`)
+      console.log('Sending request...', confirmation);
+    } catch {
+      console.error('No confirmation');
+    }
+  }
+
+  const getResponse = async (videoId) => {
+    try {
+      const _response = await axios.post(`https://sdgvr2g6eh.execute-api.eu-north-1.amazonaws.com/p/son?videoid=${videoId}`);
+      setResponse(_response);
+    } catch {
+      console.error('No response')
+    }
+
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      getResponse(id);
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [id]);
 
   return (
     <>
-          <div class="App">
-            <div class="centered">
+          <div className="App">
+            <div className="centered">
                 <p>
                     YouTube Video Content Optimization
                 </p>
-                <input id="videoUrl" type="text" value={id} placeholder="Provide a YouTube link...." onChange={handleChange} />
-                <button id="fetchButton">Optimize</button>
-                <div id="dataContainer">{id}</div>
+                <input id="videoUrl" type="text" value={url} placeholder="Provide a YouTube link...." onChange={handleChange} />
+                <button id="fetchButton" onClick={sendId}>Optimize</button>
+                <div id="dataContainer">{response}</div>
             </div>
         </div>
     </>
